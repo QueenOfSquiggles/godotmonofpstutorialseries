c47dff375a6db1c22571d7a7202711431f97f0f1
QueenOfSquiggles <8940604+QueenOfSquiggles@users.noreply.github.com>
2022-08-17 20:49:28 -0500

    Episode 4 Commit

--- modules/character-controller/CharacterController.cs
+++ modules/character-controller/CharacterController.cs
@@ -6,12 +6,22 @@ public class CharacterController : KinematicBody
 {
     // Export Variables
     [Export(PropertyHint.Range, "0.1,10.0,0.1")] public float speed = 2.0f;
+    [Export(PropertyHint.Range, "0.1,10.0,0.1")] public float sprintSpeed = 4.0f;
+    
     [Export] public float acceleration = 1.0f / 0.5f;
     [Export] public float stoppingAcceleration = 10.0f;
     [Export] public float mouseSensitivity = 0.003f;
 
+
+    [Export] public float jumpHeight = 1.0f;
+    [Export] public float jumpMaxXDistance = 2.0f;
+
+
     // Internal Member
     private Vector3 velocity = Vector3.Zero;
+    private float gravity;
+    private float jumpInitialVelocity;
+
 
     // Scene Tree Members
     private Camera camera = null;
@@ -23,6 +33,10 @@ public class CharacterController : KinematicBody
         Input.MouseMode = Input.MouseModeEnum.Captured;
         camera = GetNode<Camera>("CameraRig/YRotation/Camera");
         cameraY = GetNode<Spatial>("CameraRig/YRotation");
+
+        jumpHeight = jumpHeight / 2.0f;
+        gravity = (-2.0f * jumpHeight * Mathf.Pow(sprintSpeed, 2.0f)) / Mathf.Pow(jumpMaxXDistance, 2.0f);
+        jumpInitialVelocity = -gravity * (jumpMaxXDistance / sprintSpeed);
     }
 
     public override void _Process(float delta)
@@ -33,13 +47,16 @@ public class CharacterController : KinematicBody
 
         // init some helper variables
         var target_velocity = new Vector3();
-        var utrans = new UTransform(camera.GlobalTransform);     
+        var utrans = new UTransform(camera.GlobalTransform);
+        var current_speed = speed;
+        if (Input.IsActionPressed("sprint")) current_speed = sprintSpeed;
 
         // apply translations to the target velocity to face the relative direction
         target_velocity += utrans.Right * input.x;
         target_velocity += utrans.Forward * input.y;
         target_velocity.y = 0.0f;
-        target_velocity = target_velocity.Normalized() * speed;
+        target_velocity = target_velocity.Normalized() * current_speed;
+        target_velocity.y = velocity.y;
 
         // set up the dynamic acceleration
         var target_acceleration = acceleration;
@@ -48,6 +65,17 @@ public class CharacterController : KinematicBody
         // User interpolation to smoothly change velocity
         velocity = velocity.LinearInterpolate(target_velocity, target_acceleration * delta);
 
+        if (!IsOnFloor()) velocity.y += gravity * delta;
+        else velocity.y = 0.0f;
+
+        if (Input.IsActionPressed("jump") && IsOnFloor() && velocity.y <= 0.0f)
+        {
+            velocity.y = jumpInitialVelocity;
+        }
+        if (Input.IsActionJustReleased("jump") && !IsOnFloor() && velocity.y >= 0.0)
+        {
+            velocity.y = 0.0f;
+        }
     }
 
     public override void _Input(InputEvent e)
@@ -63,6 +91,10 @@ public class CharacterController : KinematicBody
             cameraY.RotateY(motion.x);
             camera.RotateX(motion.y);
 
+            var camRot = camera.RotationDegrees;
+            camRot.x = Mathf.Clamp(camRot.x, -70.0f, 70.0f);
+            camera.RotationDegrees = camRot;
+
         }
         if (e.IsActionPressed("ui_cancel"))
             // checks if we called the 'ui_cancel' input action. Same as Input.IsActionJustPressed("ui_cancel") in polling mode
@@ -76,6 +108,8 @@ public class CharacterController : KinematicBody
     public override void _PhysicsProcess(float delta)
         // Called once every physics frame, default is 60 frames per second. Can be changed in ProjectSettings
     {
+
+
         // uses built-in function to move the character and assign remaining velocity to the velocity member
         velocity = MoveAndSlide(velocity, Vector3.Up, true, 4, Mathf.Deg2Rad(45.0f));
     }
